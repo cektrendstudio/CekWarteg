5c1869ed08eec39c5ba707d08bf729f02553b887
dedesaepulloh <dedesaepulloh77@gmail.com>
2021-02-20 20:07:08 +0700

    temp login

--- app/src/main/java/com/cektrend/cekwarteg/LoginActivity.java
+++ app/src/main/java/com/cektrend/cekwarteg/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +17,7 @@ import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,11 +28,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button btnLogin, btnRegister;
     ProgressDialog pDialog;
     SharedPreferences sharedPreferences;
-    Boolean iSuccess = false;
+    Boolean is_approve = false;
+    Boolean session = false;
+    String userName;
     public static final String my_shared_preferences = "my_shared_preferences";
     public static final String session_status = "session_status";
     public static final String session_username = "session_username";
-    public static final String session_password = "session_password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +41,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
         initComponents();
         sharedPreferences = getSharedPreferences(my_shared_preferences, Context.MODE_PRIVATE);
-        AndroidNetworking.initialize(getApplicationContext());
+
         clickListener();
+        session = sharedPreferences.getBoolean(session_status, false);
+        if (session) {
+            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+            finish();
+        }
+        AndroidNetworking.initialize(getApplicationContext());
     }
 
     private void initComponents() {
@@ -58,10 +67,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void onClick(View view) {
         int id = view.getId();
         if (id == R.id.btn_login) {
-            String username = edtUsername.getText().toString().trim();
-            String password = edtPassword.getText().toString().trim();
-            if (!username.isEmpty() || !password.isEmpty()) {
-                login(username, password);
+            String user = edtUsername.getText().toString().trim();
+            String passwd = edtPassword.getText().toString().trim();
+            if (!user.isEmpty() || !passwd.isEmpty()) {
+                login(user, passwd);
             } else {
                 edtUsername.setError("Please insert your username");
                 edtPassword.setError("Please insert your password");
@@ -82,47 +91,39 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 .addBodyParameter("password", password)
                 .setPriority(Priority.MEDIUM)
                 .build()
-                .getAsJSONArray(new JSONArrayRequestListener() {
+                .getAsJSONObject(new JSONObjectRequestListener() {
                     @Override
-                    public void onResponse(JSONArray response) {
+                    public void onResponse(JSONObject response) {
                         hideDialog();
-                        String username = "", password = "", messages = "";
-                        try {
-                            for (int i = 0; i < response.length(); i++) {
-                                JSONObject data = response.getJSONObject(i);
-                                username = data.getString("username");
-                                password = data.getString("password");
-                                messages = data.getString("messages");
-                                iSuccess = data.getBoolean("iSuccess");
-                            }
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject data = response.getJSONObject("user");
+                                userName = data.getString("username");
+                                is_approve = data.getBoolean("is_approve");
 
-                            if (iSuccess == true) {
-                                Toast.makeText(getApplicationContext(), messages, Toast.LENGTH_SHORT).show();
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                editor.putBoolean(session_status, true);
-                                editor.putString(session_username, username);
-                                editor.putString(session_password, password);
-                                editor.apply();
-                                iSuccess = sharedPreferences.getBoolean(session_status, false);
-                                if (iSuccess) {
-                                    startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
-                                    finish();
-                                }
-                            } else {
-                                Toast.makeText(getApplicationContext(), messages, Toast.LENGTH_SHORT).show();
-                                edtUsername.setText("");
-                                edtPassword.setText("");
-                                hideDialog();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
                         }
 
+                        if (is_approve) {
+                            Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_SHORT).show();
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putBoolean(session_status, true);
+                            editor.putString(session_username, userName);
+                            editor.apply();
+                            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Gagal Login", Toast.LENGTH_SHORT).show();
+                            edtPassword.setText("");
+                            edtUsername.setText("");
+                            hideDialog();
+                        }
                     }
-
                     @Override
                     public void onError(ANError anError) {
                         Toast.makeText(getApplicationContext(), "Gagal terhubung, cobalah periksa koneksi internet anda", Toast.LENGTH_SHORT).show();
+                        Log.e("Error", "bagian ieu :", anError);
                         hideDialog();
                     }
                 });
