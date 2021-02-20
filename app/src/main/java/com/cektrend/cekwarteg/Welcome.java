f98fb17ef9b4f57a0184ee15e667e00cf8b12ffa
dedesaepulloh <dedesaepulloh77@gmail.com>
2021-02-20 14:25:41 +0700

    add function showWarteg

--- app/src/main/java/com/cektrend/cekwarteg/Welcome.java
+++ app/src/main/java/com/cektrend/cekwarteg/Welcome.java
@@ -1,6 +1,7 @@
 package com.cektrend.cekwarteg;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
@@ -9,6 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.cektrend.cekwarteg.adapter.ListWartegAdapter;
 import com.cektrend.cekwarteg.model.DataWarteg;
 
 import org.json.JSONArray;
@@ -20,6 +26,7 @@ import java.util.ArrayList;
 public class Welcome extends AppCompatActivity {
     EditText edtSearchMenu;
     ProgressDialog pDialog;
+    ListWartegAdapter adapter;
     ArrayList<DataWarteg> dataWartegs = new ArrayList<>();
     private RecyclerView recyclerView;
 
@@ -28,11 +35,60 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         initComponents();
+        AndroidNetworking.initialize(getApplicationContext());
+        recyclerView = findViewById(R.id.rv_warteg);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        showWarteg();
+
     }
 
     private void initComponents() {
         edtSearchMenu = findViewById(R.id.edt_search_menu);
 
+    }
+
+    private void showWarteg() {
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Memuat ...");
+        showDialog();
+        AndroidNetworking.get("http://103.146.203.97/api/warteg")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        hideDialog();
+                        try {
+                            for (int i = 0; i < response.length();
+                                 i++) {
+                                JSONObject data =
+                                        response.getJSONObject(i);
+                                dataWartegs.add(new DataWarteg(data.getString("code"), data.getString("name"), data.getString("email"), data.getString("owner_name"), data.getString("address"), data.getString("phone"), data.getString("description"), data.getString("photo_profile")));
+                            }
+                            adapter = new ListWartegAdapter(dataWartegs, getApplicationContext(), Welcome.this);
+                            recyclerView.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        Toast.makeText(getApplicationContext(), "Gagal memuat, cobalah periksa koneksi internet anda", Toast.LENGTH_SHORT).show();
+                        hideDialog();
+                    }
+                });
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 
 }
