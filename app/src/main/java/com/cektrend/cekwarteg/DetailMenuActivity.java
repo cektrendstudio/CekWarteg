97e4a775dcf7c41bcc4ea989683e040f7df576bf
dedesaepulloh <dedesaepulloh77@gmail.com>
2021-02-23 11:41:38 +0700

    update layout

--- app/src/main/java/com/cektrend/cekwarteg/DetailMenuActivity.java
+++ app/src/main/java/com/cektrend/cekwarteg/DetailMenuActivity.java
@@ -3,12 +3,23 @@ package com.cektrend.cekwarteg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RatingBar;
 
 public class DetailMenuActivity extends AppCompatActivity {
+    EditText edtName, edtUlas;
+    RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_menu);
+        initComponent();
+    }
+
+    private void initComponent(){
+        edtName = findViewById(R.id.edt_nama);
+        edtUlas = findViewById(R.id.edt_ulasan);
+        ratingBar = findViewById(R.id.rating_menu);
     }
 }
