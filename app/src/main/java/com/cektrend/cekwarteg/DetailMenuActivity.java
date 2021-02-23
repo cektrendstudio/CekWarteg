563e2c53178fdc14716221f8fc60353e37f53513
dedesaepulloh <dedesaepulloh77@gmail.com>
2021-02-23 12:59:44 +0700

    add function save data

--- app/src/main/java/com/cektrend/cekwarteg/DetailMenuActivity.java
+++ app/src/main/java/com/cektrend/cekwarteg/DetailMenuActivity.java
@@ -2,13 +2,27 @@ package com.cektrend.cekwarteg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONArrayRequestListener;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DetailMenuActivity extends AppCompatActivity {
     ImageView imgMenu;
@@ -16,17 +30,33 @@ public class DetailMenuActivity extends AppCompatActivity {
     RatingBar ratingMenu;
     EditText edtReviewerName, edtUlasan;
     Button btnSubmit;
+    ProgressDialog pDialog;
+    Integer idMenu;
+    Boolean isSuccess = false;
+    Integer menuId, id;
+    String name, review, message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_menu);
+        idMenu = 1;
 
         initComponents();
+
+//        ratingMenu.setRating((float) 4.5);
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //    Event Submit Review
+               String nama = edtReviewerName.getText().toString();
+               String ulas = edtUlasan.getText().toString();
+                if (!nama.isEmpty() || !ulas.isEmpty()) {
+                    saveData(nama, ulas);
+                } else {
+                    edtReviewerName.setError("Please insert your Name");
+                    edtUlasan.setError("Please insert your review");
+                }
             }
         });
     }
@@ -39,5 +69,66 @@ public class DetailMenuActivity extends AppCompatActivity {
         edtReviewerName = findViewById(R.id.edt_reviewer_name);
         edtUlasan = findViewById(R.id.edt_ulasan);
         btnSubmit = findViewById(R.id.btn_submit);
+    }
+
+    private void saveData(String nama, String ulas){
+        pDialog = new ProgressDialog(this);
+        pDialog.setCancelable(false);
+        pDialog.setMessage("Verifikasi ...");
+        showDialog();
+        AndroidNetworking.post(BuildConfig.BASE_URL + "api/menu/{menu_id}/review")
+                .addBodyParameter("menu_id", String.valueOf(1))
+                .addBodyParameter("name", nama)
+                .addBodyParameter("review_text", ulas)
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        hideDialog();
+//                        try {
+
+//                            Log.d("TAG", "info" + response.getJSONArray(0));
+
+//                            for (int i = 0; i < response.length(); i++) {
+//                                Log.d("TAG", "respon" + response);
+//                                JSONObject data = response.getJSONObject("data");
+//                                menuId = data.getInt("menu_id");
+//                                id = data.getInt("id");
+//                                name = data.getString("name");
+//                                review = data.getString("review_text");
+//                                isSuccess = response.getBoolean("isSuccess");
+//                                message = response.getString("message");
+//                            }
+//                            if (isSuccess) {
+//                                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+//
+//                            } else {
+//                                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+//                                hideDialog();
+//                            }
+//                        } catch (JSONException e) {
+//                            e.printStackTrace();
+//                            Log.e("TAG", "JSONE", e);
+//                        }
+                    }
+
+                    @Override
+                    public void onError(ANError error) {
+                        Toast.makeText(getApplicationContext(), "Gagal terhubung, cobalah periksa koneksi internet anda", Toast.LENGTH_SHORT).show();
+                        Log.e("TAG", "AnError" + error.getErrorBody());
+                        hideDialog();
+                    }
+                });
+    }
+
+    private void showDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
     }
 }
